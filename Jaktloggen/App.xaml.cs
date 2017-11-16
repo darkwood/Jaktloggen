4542e1b7e8e534261d94b3bf3187bf48e3d8be65
Tore Mørkved <tore.morkved@gmail.com>
2017-11-16 21:46:04 +0100

    renaming til mainpage

--- Jaktloggen/App.xaml.cs
+++ Jaktloggen/App.xaml.cs
@@ -4,14 +4,16 @@ namespace Jaktloggen
 {
     public partial class App : Application
     {
+        public static bool UseMockDataStore = true;
+        public static string BackendUrl = "https://localhost:5000";
         public App()
         {
             InitializeComponent();
 
             if (Device.RuntimePlatform == Device.iOS)
-                MainPage = new JaktloggenPage();
+                MainPage = new MainPage();
             else
-                MainPage = new NavigationPage(new JaktloggenPage());
+                MainPage = new NavigationPage(new MainPage());
             
         }
 

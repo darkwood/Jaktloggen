8d906d5a733c1287f9c3b6af3f30d8fc8a0f6773
Tore Mørkved <tore.morkved@gmail.com>
2017-11-16 21:31:28 +0100

    files from jlxaml

---
+++ Jaktloggen/About/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Jaktloggen
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Jaktloggen/About/AboutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace Jaktloggen
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Om Jaktloggen";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("http://www.jaktloggen.no")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
